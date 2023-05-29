49ef83ef10e50769db18886caa976656dbfa7235
Suhho Lee <suhholeemichael@gmail.com>
2023-05-29 17:04:09 +0900

    day 1 final

--- Section1/section1.js
+++ Section1/section1.js
@@ -4,6 +4,6 @@ var input2 = document.getElementById('num2');
 function add(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
+button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
